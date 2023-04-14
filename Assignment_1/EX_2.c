560e5e629a6a45e298d2c9bacd05181a21273082
Andrew Adel <andrewadel3322@gmail.com>
2023-04-14 19:04:03 +0200

    encapsulate Assignment_1

--- Assignment_1/EX_2.c
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-void EX_2(void){
-	int in;
-	printf("Enter a integer: ");
-	fflush(stdout); fflush(stdin);
-	scanf("%i", &in);
-	printf("You entered: %i", in);
-}
--- Assignment_1/EX_5.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-
-void EX_5(){
-	char in;
-	printf("Enter a character: ");
-	fflush(stdout); fflush(stdin);
-	scanf("%c", &in);
-	printf("ASCII value of %c = %d", in,in);
-}
