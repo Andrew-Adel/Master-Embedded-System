f06939958c84d7a8beeabf669575d15ba400b81f
Andrew Adel <andrewadel3322@gmail.com>
2023-04-14 21:00:21 +0200

    update

--- C_Programming/Assignment_1/EX_1.c
+++
@@ -1,5 +0,0 @@
-#include "stdio.h"
-void EX_1(){
-	printf("C Programming");
-
-}
--- C_Programming/Assignment_1/EX_4.c
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-void EX_4(){
-	float in1, in2;
-	printf("Enter two numbers: ");
-	fflush(stdout); fflush(stdin);
-	scanf("%f %f", &in1, &in2);
-	printf("Sum: %f", in1 * in2);
-}
